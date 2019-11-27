c2c727925e73080af1384f34d711d1bf322c3eae
Sylvain Huss <sylvain.huss@laposte.net>
2019-11-27 17:23:56 +0100

    crontab works

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ up:
 	sudo docker-compose up -d
 	nohup sudo docker exec API npm run --prefix /usr/app/ watch > output.log &
 	@echo "API server is running!"
-	sudo docker exec quality service cron restart
+	sudo docker exec quality chmod 755 /home/script.sh
+	sudo docker exec quality service cron start
 
 down:
 	sudo docker-compose down
--- quality/quality.py
+++ quality/quality.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import requests
 import json
 from random import *
@@ -6,18 +8,18 @@ try:
     r = requests.get("http://172.16.0.6:3000/api/radios", timeout=5, verify=False, stream=True)
     length = len(r.json())
 
-    idx = randint(0,length)
-    r = requests.get("http://172.16.0.6:3000/api/radios/" + str(idx), timeout=5, verify=False, stream=True)
+    id = randint(0,length)
+    r = requests.get("http://172.16.0.6:3000/api/radios/" + str(id), timeout=5, verify=False, stream=True)
     radioUrl = (r.json())['url']
     radioName = (r.json())['title']
     
     try:
-        requests.get(radioUrl, timeout=10, verify=False, stream=True)
-        requests.get("http://172.16.0.6:3000/api/radios/" + str(idx) + "/up", timeout=10, verify=False, stream=True)
+        r = requests.get(radioUrl, timeout=10, verify=False, stream=True)
+        r = requests.get("http://172.16.0.6:3000/api/radios/" + str(id) + "/up", timeout=10, verify=False, stream=True)
         print (radioName + " is up")
 
     except:
-        requests.get("http://172.16.0.6:3000/api/radios/" + str(idx) + "/down", timeout=10, verify=False, stream=True)
+        requests.get("http://172.16.0.6:3000/api/radios/" + str(id) + "/down", timeout=10, verify=False, stream=True)
         print (radioName + " is down")
 
 except:
