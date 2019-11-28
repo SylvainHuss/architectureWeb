c0db17829ee296e5ba5abe4870c83e0349f2a12e
Sylvain Huss <sylvain.huss@laposte.net>
2019-11-28 21:21:23 +0100

    quality date log

--- quality/quality.py
+++ quality/quality.py
@@ -3,24 +3,29 @@
 import requests
 import json
 from random import *
+from datetime import datetime
 
 try:
     r = requests.get("http://172.16.0.6:3000/api/radios", timeout=5, verify=False, stream=True)
     length = len(r.json())
+    print (datetime.now().strftime("%d/%m/%Y %H:%M:%S") + " - ")
 
-    id = randint(0,length)
-    r = requests.get("http://172.16.0.6:3000/api/radios/" + str(id), timeout=5, verify=False, stream=True)
-    radioUrl = (r.json())['url']
-    radioName = (r.json())['title']
-    
-    try:
-        r = requests.get(radioUrl, timeout=10, verify=False, stream=True)
-        r = requests.get("http://172.16.0.6:3000/api/radios/" + str(id) + "/up", timeout=10, verify=False, stream=True)
-        print (radioName + " is up")
+    for i in range (0,3):
+        id = randint(0,length)
+        r = requests.get("http://172.16.0.6:3000/api/radios/" + str(id), timeout=5, verify=False, stream=True)
+        radioUrl = (r.json())['url']
+        radioName = (r.json())['title']
+        
+        try:
+            r = requests.get(radioUrl, timeout=5, verify=False, stream=True)
+            r = requests.get("http://172.16.0.6:3000/api/radios/" + str(id) + "/up", timeout=5, verify=False, stream=True)
+            print (radioName + " is up")
 
-    except:
-        requests.get("http://172.16.0.6:3000/api/radios/" + str(id) + "/down", timeout=10, verify=False, stream=True)
-        print (radioName + " is down")
+        except:
+            requests.get("http://172.16.0.6:3000/api/radios/" + str(id) + "/down", timeout=5, verify=False, stream=True)
+            print (radioName + " is down")
+        
+        print (", ")
 
 except:
     print ("API disconnected")
